f3480b43cadaa66e5aa1cb528d2d9d0500d97e50
Mojak01 <Mojtaba.akbari2001@hotmail.com>
2026-02-11 20:26:22 +0100

    Lägg till inmatning av bokuppgifter

--- BokhandelAdminstration/Services/Bookservices.cs
+++ BokhandelAdminstration/Services/Bookservices.cs
@@ -10,5 +10,20 @@ namespace BokhandelAdminstration.Services
         {
             _context = new BookStoreContext();
         }
+
+        public async Task SkapaNyBokAsync()
+        {
+            Console.WriteLine("Ange ISBN (13 siffror):");
+            string isbn = Console.ReadLine();
+
+            Console.WriteLine("Ange titel:");
+            string titel = Console.ReadLine();
+
+            Console.WriteLine("Ange pris:");
+            decimal pris = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ange utgivningsdatum (yyyy-mm-dd):");
+            DateTime utgivningsdatum = DateTime.Parse(Console.ReadLine());
+        }
     }
 }
